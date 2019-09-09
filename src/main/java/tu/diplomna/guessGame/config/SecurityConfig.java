10b6e2f762039c14543e0a51dee3b5627c2d2b69
troqta <troqta@gmail.com>
2019-09-09 17:31:34 +0300

    Added temporary front page post style

--- src/main/java/tu/diplomna/guessGame/config/SecurityConfig.java
+++ src/main/java/tu/diplomna/guessGame/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/register", "/css/**", "/scripts/**", "/api/**", "/signin/**", "/signup/**").permitAll()
+                .antMatchers("/", "/register", "/css/**", "/scripts/**", "/api/**", "/signin/**", "/upload-dir/**", "/signup/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login").permitAll()
